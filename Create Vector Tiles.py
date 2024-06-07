496ab3f33381a6c75323c48e4843f4e51d5a2385
KeithVanGraafeiland <kvangraafeiland@esri.com>
2024-06-07 20:28:52 +0000

    Modified for June 2024 Pre-U.C. Release

--- Create Vector Tiles.py
+++ Create Vector Tiles.py
@@ -6,14 +6,14 @@ arcpy.env.overwriteOutput = True
 # Define Variables
 aprx = arcpy.mp.ArcGISProject(r"C:\Users\Administrator\Documents\ArcGIS\Projects\AIS\AIS.aprx")
 symbology_layer = r"D:\AIS_Processing\Vessel Traffic Layer Template Monthly.lyrx"
-vt_folder = r"D:\AIS_Processing\Vector_Tiles"
+vt_folder = r"D:\AIS_Processing\Vector_Tiles\R_03"
 vt_index = r"D:\AIS_Processing\Vector_Tile_Index.gdb\AIS_Vector_Tile_Index"
-tracks_db = r"D:\AIS_Processing\Monthly_Vessel_Tracks_v2.gdb"
-
+tracks_db = r"D:\AIS_Processing\Monthly_Vessel_Tracks_by_Group.gdb"
+vt_index_gdb = r"D:\AIS_Processing\Vector_Tile_Index.gdb"
 
 arcpy.env.workspace = tracks_db
 tracks_list = []
-for fc in arcpy.ListFeatureClasses():
+for fc in arcpy.ListFeatureClasses("*2016_01*"):
     tracks_list.append(fc)
 print(tracks_list)
 
@@ -22,17 +22,28 @@ def create_vector_tiles():
     print(vt_map.name)
     for track in tracks_list:
         track_name = tracks_db + "\\" + track
+        index_name = vt_index_gdb + "\\" + track + "index"
         vt_map.addDataFromPath(track_name)
         fc_layer = vt_map.listLayers()[0]
         vt_package = vt_folder + "\\" + track + ".vtpk"
         summary = "Monthly AIS Vessel Traffic Vector Tiles for " + track
-        tags = "AIS, Vessel Traffic, Shipping"
-        credits = "NOAA, BOEM, USGS, Marine Cadastre, Esri"
+        tags = "AIS, Vessel Traffic, U.S. Vessel Traffic"
+        credits = "NOAA, BOEM, USCG, Marine Cadastre, Esri"
+        print(track_name)
+        print(index_name)
         print(fc_layer)
-        arcpy.management.ApplySymbologyFromLayer(fc_layer,symbology_layer,"VALUE_FIELD vessel_group vessel_group", "MAINTAIN")
+        print(vt_package)
+        # arcpy.management.ApplySymbologyFromLayer(fc_layer,symbology_layer,"VALUE_FIELD vessel_group vessel_group", "MAINTAIN")
         aprx.save()
+        arcpy.management.CreateVectorTileIndex(
+            in_map=vt_map,
+            out_featureclass=index_name,
+            service_type="ONLINE",
+            tiling_scheme=None,
+            vertex_count=10000
+        )
         arcpy.management.CreateVectorTilePackage(vt_map, vt_package, "ONLINE", "", "INDEXED", 295828763.795777, 564.248588, vt_index, summary, tags)
-        #arcpy.management.SharePackage(vt_package, username, password, summary, tags, credits, "MYGROUPS", "Vessel Traffic","MYORGANIZATION", "TRUE", "AIS Vector Tiles")
+        # arcpy.management.SharePackage(vt_package, username, password, summary, tags, credits, "MYGROUPS", "Vessel Traffic","MYORGANIZATION", "TRUE", "AIS Vector Tiles")
         vt_map.removeLayer(fc_layer)
         aprx.save()
 
--- Publish Vector Tiles.py
+++ Publish Vector Tiles.py
@@ -5,7 +5,7 @@ from credentials import username, password
 ## Tested in ArcGIS Pro 2.8.3 (Released)
 
 # Define Variables
-vt_folder = r"D:\AIS_Processing\Vector_Tiles\R_02"
+vt_folder = r"D:\AIS_Processing\Vector_Tiles\R_03"
 
 arcpy.env.workspace = vt_folder
 tile_list = os.listdir(vt_folder)
@@ -16,10 +16,11 @@ def publish_vector_tiles():
         print("Publishing " + tile)
         year = tile.split("_")[3]
         month = tile.split("_")[4]
+        group = tile.split("_")[5]
         vt_package = vt_folder + "\\" + tile
-        summary = "Monthly AIS Vessel Traffic Vector Tiles for " + year + " " + month
+        summary = "Monthly AIS Vessel Traffic Vector Tiles for " + year + " " + month + " " + group
         tags = "AIS, Vessel Traffic, Shipping"
-        credits = "NOAA, BOEM, USGS, Marine Cadastre, Esri"
+        credits = "NOAA, BOEM, USCG, Marine Cadastre, Esri"
         arcpy.management.SharePackage(vt_package, username, password, summary, tags, credits, "MYGROUPS", "Vessel Traffic","MYORGANIZATION", "TRUE", "AIS Vector Tiles")
 
 
