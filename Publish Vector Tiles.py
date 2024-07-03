2dfbe59aa8444b8229cb818bbef675ce236eb0f2
Keith VanGraafeiland <kvangraafeiland@esri.com>
2024-07-03 15:01:23 -0400

    generalizing tracks and vector tils

--- Publish Vector Tiles.py
+++ Publish Vector Tiles.py
@@ -5,7 +5,7 @@ from credentials import username, password
 ## Tested in ArcGIS Pro 2.8.3 (Released)
 
 # Define Variables
-vt_folder = r"E:\ArcGIS\Projects\AIS\processing\Vector_Tiles\Test"
+vt_folder = r"E:\ArcGIS\Projects\AIS\processing\Vector_Tiles\2016"
 
 tile_list = []
 for file in os.listdir(vt_folder):
@@ -22,10 +22,11 @@ def publish_vector_tiles():
         print("Publishing " + tile)
         year = tile.split("_")[3]
         month = tile.split("_")[4]
-        group = tile.split("_")[5]
+        # group = tile.split("_")[5]
         vt_package = vt_folder + "\\" + tile
-        summary = "Monthly AIS Vessel Traffic Vector Tiles for " + year + " " + month + " " + group
-        tags = "AIS, Vessel Traffic, Shipping"
+        # summary = "Monthly AIS Vessel Traffic Vector Tiles for " + year + " " + month + " " + group
+        summary = "Monthly AIS Vessel Traffic Vector Tiles for " + year + " " + month
+        tags = "AIS, Vessel Traffic, Shipping, ocean, marine, marine spatial planning"
         credits = "NOAA, BOEM, USCG, Marine Cadastre, Esri"
         arcpy.management.SharePackage(vt_package, username, password, summary, tags, credits, "MYGROUPS", "Vessel Traffic","MYORGANIZATION", "TRUE", "AIS Vector Tiles")
 
--- metadata_config.py
+++ metadata_config.py
@@ -3,3 +3,7 @@ md_description = "Monthly AIS Vessel Traffic Hexbin Summaries including total mo
 # md_summary = "Monthly AIS Vessel Traffic Hexbin Summaries for"
 md_tags = "AIS, vessel traffic, U.S. Vessel Traffic, marine spatial planning, oceans, marine, esri_oceans, esri_environment, law_hosted"
 md_useLimitations = "These are use limitations"
+
+esri_public = """
+<img alt="" src="https://downloads.esri.com/blogs/arcgisonline/esrilogo_new.png" /> This work is licensed under the Esri Master License Agreement.<br /><div><a href="https://goto.arcgis.com/termsofuse/viewsummary" target="_blank"><b>View Summary</b></a> | <a href="https://goto.arcgis.com/termsofuse/viewtermsofuse" target="_blank"><b>View Terms of Use</b></a></div>
+"""
